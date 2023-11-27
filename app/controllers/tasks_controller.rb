757d6494de3e6afedb25020c39936090cc21403a
Julia Kleber <julia.kleber@gmx.net>
2023-11-27 16:47:27 +0100

    added the routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  def todays_tasks
+  end
+
+  def index
+    @tasks = Task.where(user_id: current_user)
+    @dates = Set.new([])
+    @tasks.each do |task|
+      @dates << task.due_date
+    end
+    @grouped_tasks = []
+    @dates.each do |date|
+      dated_tasks = @tasks.select { |task| task.due_date = date }
+      @grouped_tasks << dated_tasks
+    end
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Clearing the database ...'
+
+TaskCategory.destroy_all
+User.destroy_all
+Task.destroy_all
+Category.destroy_all
+
+puts 'Creating users ...'
+
+# Create users
+user_names = ['Aisiri', 'Bilal', 'Elena', 'Julia']
+
+users = user_names.map do |name|
+  User.create(user_name: name, email: "#{name.downcase}@todocoach.com", password: '123456')
+end
+
+puts User.all
+
+# Create categories for each user
+categories = users.flat_map do |user|
+  ['Work', 'Personal', 'Study'].map do |category_name|
+    user.categories.create(name: category_name)
+  end
+end
+
+# Create tasks for each user due in the next two weeks and assign categories
+users.each do |user|
+  30.times do |n|
+    due_date = Time.now + (n / 3).days
+    task = user.tasks.create(
+      title: "Task #{n + 1} for #{user.user_name}",
+      description: "Description for task #{n + 1}",
+      priority: rand(1..3),
+      completed: false,
+      due_date: due_date,
+      reminder_date: due_date - 1.day
+    )
+    # Assign random categories to the task
+    task.categories << categories.sample(rand(1..3))
+  end
+end
