b8ba906f2d5a7c677e001e2456e5ea0577c53ff2
Julia Kleber <julia.kleber@gmx.net>
2023-12-01 16:03:33 +0100

    users controller, feed and connect actions and views added

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,18 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
   devise_for :users
+
+  resources :users, except: %i[index new create edit update destroy] do
+    member do
+      get :feed
+      get :connect
+      post :build_connection
+    end
+  end
+
   root to: 'pages#home'
   get 'pages/reminder', to: 'pages#reminder'
+  
   resources :tasks do
     collection do
       get :todays_tasks, as: 'todays'
